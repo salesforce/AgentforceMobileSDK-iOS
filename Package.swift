7c0417fca0f9e240903da09bc99ab337ea6114e2
Alex Sikora <asikora@salesforce.com>
2026-03-12 15:39:11 -0500

    Add swift_markdown_ui shim for SPM module name compatibility

--- Package.swift
+++ Package.swift
@@ -24,16 +24,24 @@ let package = Package(
             url: "https://github.com/salesforce/AgentforceMobileSDK-iOS/releases/download/15.0.7/AgentforceMobileSDK-260-1.xcframework.zip",
             checksum: "f034aef2e233cf06ff0cb6e9896e797e1d783d53ad11ca503a575fbf138d0275"
         ),
+        // Shim: the binary imports "swift_markdown_ui" but SPM builds it as "MarkdownUI"
+        .target(
+            name: "swift_markdown_ui",
+            dependencies: [
+                .product(name: "MarkdownUI", package: "swift-markdown-ui"),
+            ],
+            path: "Sources/swift_markdown_ui"
+        ),
         .target(
             name: "AgentforceSDKTarget",
             dependencies: [
                 "AgentforceSDK",
+                "swift_markdown_ui",
                 .product(name: "AgentforceService", package: "AgentforceMobileService-iOS"),
                 .product(name: "SalesforceNavigation", package: "SalesforceMobileInterfaces-iOS"),
                 .product(name: "SalesforceLogging", package: "SalesforceMobileInterfaces-iOS"),
                 .product(name: "SalesforceNetwork", package: "SalesforceMobileInterfaces-iOS"),
                 .product(name: "SalesforceUser", package: "SalesforceMobileInterfaces-iOS"),
-                .product(name: "MarkdownUI", package: "swift-markdown-ui"),
             ],
             path: "Sources/AgentforceSDKTarget"
         )
