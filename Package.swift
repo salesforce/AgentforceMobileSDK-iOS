640f2024b895bd583d348bb1d4e412debd2eb2e8
Alex Sikora <asikora@salesforce.com>
2026-03-12 15:08:45 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "AgentforceMobileSDK",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "AgentforceSDK",
+            targets: ["AgentforceSDKTarget"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/forcedotcom/AgentforceMobileService-iOS.git", from: "4.0.0"),
+        .package(url: "https://github.com/forcedotcom/SalesforceMobileInterfaces-iOS.git", from: "1.0.0"),
+        .package(url: "https://github.com/salesforce-misc/swift-markdown-ui.git", from: "2.4.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AgentforceSDK",
+            url: "https://github.com/salesforce/AgentforceMobileSDK-iOS/releases/download/15.0.7/AgentforceMobileSDK-260-1.xcframework.zip",
+            checksum: "f034aef2e233cf06ff0cb6e9896e797e1d783d53ad11ca503a575fbf138d0275"
+        ),
+        .target(
+            name: "AgentforceSDKTarget",
+            dependencies: [
+                "AgentforceSDK",
+                .product(name: "AgentforceService", package: "AgentforceMobileService-iOS"),
+                .product(name: "SalesforceNavigation", package: "SalesforceMobileInterfaces-iOS"),
+                .product(name: "SalesforceLogging", package: "SalesforceMobileInterfaces-iOS"),
+                .product(name: "SalesforceNetwork", package: "SalesforceMobileInterfaces-iOS"),
+                .product(name: "SalesforceUser", package: "SalesforceMobileInterfaces-iOS"),
+                .product(name: "MarkdownUI", package: "swift-markdown-ui"),
+            ],
+            path: "Sources/AgentforceSDKTarget"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
