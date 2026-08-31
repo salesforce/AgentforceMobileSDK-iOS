66b5f9efa6579d7059114fde2b2742acc2cd736f
Alex Sikora <asikora@salesforce.com>
2026-08-31 16:06:52 -0500

    Add SPM binary distribution manifest for 18.26.23-rc7 (262.1.4-RC7 RC)

--- Sources/AgentforceCustomizationTarget/Exports.swift
+++
@@ -1,6 +0,0 @@
-// Wrapper target source. Re-exports the AgentforceCustomization binary so
-// consumers who `import AgentforceCustomization` (the binary module name) get
-// every public symbol, while SwiftPM links the binary plus the transitive SPM
-// dependencies declared on this target.
-
-@_exported import AgentforceCustomization
